6933b15cf32905e2b124918222c9f41891fd1b38
Hyunsoo Jung <sunni303356@naver.com>
2026-05-14 18:35:08 +0900

    [level 1] Title: 예산, Time: 0.34 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+        int currentBudget = 0;
+        int answer = 0;
+        for(int i = 0; i < d.length; i++){
+            if(currentBudget < budget && (d[i] + currentBudget <= budget)){
+                currentBudget += d[i];
+                answer ++;
+            }
+        }
+        return answer;
+    }
+}
