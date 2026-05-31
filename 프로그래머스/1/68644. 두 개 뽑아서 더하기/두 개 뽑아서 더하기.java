4a9fb5fd6e0b2b770015133286392f535e34752b
Hyunsoo Jung <sunni303356@naver.com>
2026-05-31 22:40:39 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 2.74 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -1,18 +1,37 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
-        Set<Integer> treeSet = new TreeSet<>(); //중복x, 정렬
-        for(int i = 0; i < numbers.length; i++){
-            for(int j = i + 1; j < numbers.length; j++){
-                treeSet.add(numbers[i] + numbers[j]);
+        //list
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                int sum = numbers[i] + numbers[j];
+
+                if (!list.contains(sum)) {
+                    list.add(sum);
+                }
             }
         }
+        Collections.sort(list); //Arrays.sort 오름차순 정렬
         
-        int[] answer = new int[treeSet.size()];
-        int index = 0;
-        for(int number: treeSet){
-            answer[index++] = number;
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
         }
+        //set
+//         Set<Integer> treeSet = new TreeSet<>(); // 중복 제거 + 오름차순 정렬
+
+//         for (int i = 0; i < numbers.length; i++) {
+//             for (int j = i + 1; j < numbers.length; j++) {
+//                 treeSet.add(numbers[i] + numbers[j]);
+//             }
+//         }
+
+//         int[] answer = new int[treeSet.size()];
+//         int index = 0;
+//         for (int number : treeSet) {
+//             answer[index++] = number;
+//         }
         return answer;
     }
 }
