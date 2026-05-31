522a1743d52083e2ce4892b32bf2bbedaf564d9a
Hyunsoo Jung <sunni303356@naver.com>
2026-06-01 00:43:29 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.07 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,9 +2,8 @@ class Solution {
     public int solution(String s) {
         String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         for(int i = 0; i < words.length; i++){
-            s = s.replace(words[i], String.valueOf(i));
+            s = s.replace(words[i], String.valueOf(i)); //"zero"->"0"
         }
-        int answer = Integer.parseInt(s);
-        return answer;
+        return Integer.parseInt(s); //"1478"->1478
     }
 }
