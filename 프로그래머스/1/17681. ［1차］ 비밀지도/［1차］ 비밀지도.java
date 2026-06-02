2d1aa8ea0adec789c798c4b3ccd94c012dfaa8e5
Hyunsoo Jung <sunni303356@naver.com>
2026-06-02 23:48:10 +0900

    [level 1] Title: [1차] 비밀지도, Time: 3.00 ms, Memory: 64.9 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,9 +2,9 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         for(int i = 0; i < n; i++){
-            String binary = Integer.toBinaryString(arr1[i] | arr2[i]);
-            binary = String.format("%" + n + "s", binary).replace(" ", "0");
-            answer[i] = binary.replace("1", "#").replace("0", " ");
+            String binary = Integer.toBinaryString(arr1[i] | arr2[i]); //2진수로 변환 31->11111
+            binary = String.format("%" + n + "s", binary).replace(" ", "0"); //n자리수, 첫공백을 0으로 변환
+            answer[i] = binary.replace("1", "#").replace("0", " "); //1은 #, 0은 " "으로 변환
         }
         
         return answer;
